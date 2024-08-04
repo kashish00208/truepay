65b14bc21db8918dc6b07694f82551a2a24109f4
kashish00208 <kgupta7827@gmail.com>
2024-08-04 22:04:24 +0530

    Error resolved

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Nav/navbar"
+import Navbar from "@/components/Nav/Navbar"
 
 const inter = Inter({ subsets: ["latin"] });
 
